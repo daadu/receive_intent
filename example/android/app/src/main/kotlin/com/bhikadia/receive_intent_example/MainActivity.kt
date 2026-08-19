184518ff66b8d5f7e1135add0c169124281c4bb4
Harsh Bhikadia <harsh@bhikadia.com>
2026-08-19 14:14:25 +0530

    feat!: upgrade the toolchain
    - dropped support for flutter <3.3
    - sync with the plugin template from flutter_tool

--- example/android/app/src/main/kotlin/com/bhikadia/receive_intent_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/bhikadia/receive_intent_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.bhikadia.receive_intent_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
